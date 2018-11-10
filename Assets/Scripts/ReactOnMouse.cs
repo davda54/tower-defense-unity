9aaaefdd6b2585b3500e2a64295d5b6e594d4d97
David Samuel <david.samuel@seznam.cz>
2018-11-10 23:47:30 +0100

    initial commit

---
+++ Assets/Scripts/ReactOnMouse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactOnMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        gameObject.transform.Translate(0, -2f, 0);
+    }
+
+    void OnMouseUp()
+    {
+        gameObject.transform.Translate(0, 2f, 0);
+    }
+}
