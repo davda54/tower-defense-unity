d605d669a96cee5d620360a6d8c712cc5c704378
David Samuel <david.samuel@seznam.cz>
2019-02-19 18:40:21 +0100

    automatic disable script

---
+++ Assets/Scripts/AutoDisableScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AutoDisableScript : MonoBehaviour
+{
+    public float TimeOut;
+    private float remainingTime;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    remainingTime = TimeOut;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (remainingTime < 0) gameObject.SetActive(false);
+	    else remainingTime -= Time.deltaTime;
+	}
+}
