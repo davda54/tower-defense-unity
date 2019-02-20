8957d9f8102e576ad2a2af165496e6e8ad2659ca
David Samuel <david.samuel@seznam.cz>
2019-02-20 16:13:42 +0100

    Pause button

---
+++ Assets/Scripts/PauseButtonScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseButtonScript : MonoBehaviour
+{
+    public GameObject PauseButton;
+    public GameObject PausePanel;
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0.0f;
+
+        PauseButton.SetActive(false);
+        PausePanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+
+        PausePanel.SetActive(false);
+        PauseButton.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Menu_screen");
+    }
+}
